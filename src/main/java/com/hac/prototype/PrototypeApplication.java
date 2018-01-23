a13f13cba4167c7499d52f9394496c2bb0c4f18e
Wutthichai Chumsaengchotsakul <wutthichai.chu@ascendcorp.com>
2018-01-23 11:15:24 +0700

    update ignore file

---
+++ src/main/java/com/hac/prototype/PrototypeApplication.java
@@ -0,0 +1,12 @@
+package com.hac.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+}
